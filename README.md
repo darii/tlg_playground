2ecaee44e88497947dfbe2d7316867393744cc45
darii <github@darii.de>
2019-04-23 19:30:32 +0300

    A first ever draft with basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # tlg_playground
 **tlg_playground** is a dummy project for a telegram bot written on Typescript.
+
+## tl;dr?
+Create an `.env` file in the root of this project with the following info:
+
+```$sh
+TOKEN=<your telegram token>
+RT_LOGIN=<your rutracker login>
+RT_PASSW=<your rutracker password>
+LOG_LEVEL=3
+```
+
+For Telegram API key, check https://github.com/Finalgalaxy/telegram-telegraf-bot and follow README instructions about how to create a Telegram Bot.
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,63 @@
 import * as dotenv from 'dotenv'
-import {Telegraf, ContextMessageUpdate} from "telegraf";
-const telegraf = require('telegraf');
 dotenv.config({path: `${__dirname}/../.env`});
+
+import {Telegraf, ContextMessageUpdate} from "telegraf";
+const RutrackerApi = require('rutracker-api');
+const telegraf = require('telegraf');
+const extra = require('telegraf/extra')
+const markup = extra.markdown()
+const rutracker = new RutrackerApi();
 const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN);
+
+rutracker.login({username: process.env.RT_LOGIN, password: process.env.RT_PASSW})
+    .then(() => {
+        console.log('Authorized');
+    })
+    .catch(err => console.error(err));
+
+bot.command('ok', ctx => {
+    ctx.replyWithSticker('CAADAgAD_AADyxkPAAFY-7LhMUQGpgI')
+        .catch(err => console.error(err));
+});
 bot.on('text', ctx => {
     console.log('@' + ctx.message.from.username + ': "' + ctx.message.text + '"');
-    // do action here
-    // ctx.reply(ctx.message.text)
+    let searchTerm: string = ctx.message.text.substring(8);
+    let title: string;
+    let torrentId: string;
+    let category: string;
+    let state: string;
+    let seeds: number;
+    let response:string;
+    rutracker.search({query: searchTerm, sort: 'seeds', order: 'desc'})
+        .then(torrents => {
+            torrents.forEach((item, index) => {
+                if (index < 5) {
+                    console.log(index + 1, item.title, item.seeds)
+                }
+            });
+            torrentId = torrents[0].id;
+            title = torrents[0].title;
+            category = torrents[0].category;
+            state = torrents[0].state;
+            seeds = torrents[0].seeds;
+            response = title + ' (' + state + ') в категории "' + category + '", ' + seeds.toString() + ' seeds.';
+            ctx.reply(response)
+                .catch(err => console.error(err)
+                );
+            rutracker.getMagnetLink(torrentId)
+                .then(
+                    uri => ctx.reply('```\n' +  uri + '\n```', markup)
+                )
+                .catch(err => console.error(err));
+        })
+        .catch(err => {
+            console.log('Bad query, cannot find anything special :(');
+            ctx.replyWithSticker('CAADAgAD-gADyxkPAAFbFzf_Nf8KeQI');
+        });
+});
+
+bot.on('sticker', ctx => {
+    ctx.reply('👍')
 });
 bot.startPolling();
-console.log('Welcome to ' + process.env.APP_ID);
+
